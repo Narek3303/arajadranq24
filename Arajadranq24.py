41862c2d594b7de8184e01d82dab8065736b6869
Narek <margaryannarek056@gmail.com>
2023-09-22 13:36:44 +0400

    h

---
+++ Arajadranq24.py
@@ -0,0 +1,58 @@
+import math
+import datetime
+import time
+
+# ex 1
+# def shrjan(r, alpha):
+#     alpha = math.degrees(alpha)
+#     S = (math.pi * r ** 2) * alpha / 360
+#     print(S)
+# shrjan(10, 6)
+
+
+# print(datetime.timedelta(weeks=))
+
+# # ex 2
+# def f(year, month, day):
+#     p = {1: 'erk', 2: 'ereq', 3: 'choreq', 4: 'hingsh', 5: 'urbat', 6: 'shabat', 0: 'kiraki'}
+#     d = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6:30, 7:31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+#     if year % 400 == 0 or year % 100 != 0 and year % 4 == 0:
+#         d[2] = 29
+#         g = []
+#         r = 0
+#         for i, j in d.items():
+#             if month == i:
+#                 r += (sum(g) + day + year - 1) % 7
+#             else:
+#                 g.append(j)
+#         return p[r]
+#     else:
+#         g = []
+#         r = 0
+#         for i, j in d.items():
+#             if month == i:
+#                 r += (sum(g) + day + year - 1) % 7
+#             else:
+#                 g.append(j)
+#         return p[r]
+#
+# print(f(2020, 1, 16))
+
+# 3
+
+
+
+
+# chi stacvel
+# def f(a: int):
+#     k = ''
+#     d = str(a)
+#     l = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'IIX', 9: 'IX', 10: 'X'}
+#     e = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
+#     for i in d:
+#         for j, m in e.items():
+#
+#
+
+
+
